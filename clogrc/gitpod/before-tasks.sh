9c263b11a86657feb6d2123c29debe0498dd4a30
Bruce Devlin <bruce@mrmxf.com>
2022-04-22 15:52:53 +0000

    fixed gitpod boot

---
+++ clogrc/gitpod/before-tasks.sh
@@ -0,0 +1,26 @@
+#                       _
+#   ___  _ __    _ __  | |_   ___   ___   _ _   __ _
+#  (_-< | '  \  | '_ \ |  _| / -_) |___| | '_| / _` |
+#  /__/ |_|_|_| | .__/  \__| \___|       |_|   \__,_|
+#               |_|
+source $GITPOD_REPO_ROOT/clogrc/core/mm-core-inc.sh
+echo -e "${Cgreen}gitpod$cC BEFORE$cT tasks starting"
+# ------------------------------------------------------------------------------
+
+fnInfo "$cC yarn$cT global path"
+PATH="$PATH:$(yarn global bin)"
+echo "PATH=\"$PATH\"" >> ~/.bashrc
+
+fnInfo "$cC  zmp$cT update"
+sudo wget --quiet --output-document=/usr/local/bin/zmp  https://mrmxf.com/get/zmp  ; sudo chmod +x /usr/local/bin/zmp
+
+fnInfo "$cC clog$cT update"
+sudo wget --quiet --output-document=/usr/local/bin/clog https://mrmxf.com/get/clog ; sudo chmod +x /usr/local/bin/clog
+
+fnInfo "$cC  aws$cT cli install"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip -q -o /tmp/awscliv2.zip -d /tmp
+sudo /tmp/aws/install
+
+# ------------------------------------------------------------------------------
+fnDivider
